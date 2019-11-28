7f7a10612de013894cf2429a7f36783728eb5b6b
Hamid Mosalla <hmosallanejad@talentsoft.com>
2019-11-28 17:46:44 +0100

    first commit

---
+++ Services/AccessTokenService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Requests;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+
+namespace GoogleIndexingAPI.Services
+{
+    public class GoogleIndexingApiService
+    {
+        public async Task<string> GetAccessTokenWithJsonPrivateKey()
+        {
+            var privateKeyStream = File.OpenRead(@"C:\Users\hmosallanejad\Desktop\New folder\hamidmosalla-83e0e56ac909.json");
+
+            var serviceAccountCredential = ServiceAccountCredential.FromServiceAccountData(privateKeyStream);
+
+            var googleCredetial = GoogleCredential.FromServiceAccountCredential(serviceAccountCredential);
+
+            var result = await googleCredetial.UnderlyingCredential.GetAccessTokenForRequestAsync("https://www.googleapis.com/auth/indexing");
+
+            var request = new BatchRequest(new GoogleIndexingService());
+
+            return result;
+        }
+
+        public async Task<string> GetAccessTokenWithP12PrivateKey()
+        {
+            var serviceAccountEmail = "hamidmosalla3@hamidmosalla.iam.gserviceaccount.com";
+
+            var certificate = new X509Certificate2(@"C:\Users\hmosallanejad\Desktop\New folder\hamidmosalla-8ba5b4d3d59a.p12",
+                "notasecret", X509KeyStorageFlags.Exportable);
+
+            var serviceAccountCredential = new ServiceAccountCredential(
+                new ServiceAccountCredential.Initializer(serviceAccountEmail)
+                    .FromCertificate(certificate));
+
+            var googleCredetial = GoogleCredential.FromServiceAccountCredential(serviceAccountCredential);
+
+            var result = await googleCredetial.UnderlyingCredential.GetAccessTokenForRequestAsync("https://www.googleapis.com/auth/indexing");
+
+            return result;
+        }
+
+        public async Task<HttpStatusCode> SendIndexUpdateRequest(string url)
+        {
+            // 
+
+
+            return await Task.FromResult(HttpStatusCode.OK);
+        }
+
+        public async Task<HttpStatusCode> GetNotificationStatus()
+        {
+            var client = new RestClient("https://indexing.googleapis.com/v3/urlNotifications/metadata");
+            var request = new RestRequest(Method.GET);
+            var accessToken = GetAccessTokenWithJsonPrivateKey();
+            request.AddHeader("Authorization", $"Bearer {accessToken}");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("key", "AIzaSyAtVs5Fje6k8reFgAqkswERrYKGuhBFsQw");
+            IRestResponse response = client.Execute(request);
+
+            return await Task.FromResult(response.StatusCode);
+        }
+    }
+}
---
+++ Services/GoogleIndexingService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Google.Apis;
+using Google.Apis.Http;
+using Google.Apis.Requests;
+using Google.Apis.Services;
+
+namespace GoogleIndexingAPI.Services
+{
+    public class GoogleIndexingService : IClientService
+    {
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetRequestSerailizedContent(HttpRequestMessage request, object body)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string SerializeObject(object data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> DeserializeResponse<T>(HttpResponseMessage response)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RequestError> DeserializeError(HttpResponseMessage response)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ConfigurableHttpClient HttpClient { get; }
+        public IConfigurableHttpClientInitializer HttpClientInitializer { get; }
+        public string Name { get; }
+        public string BaseUri { get; }
+        public string BasePath { get; }
+        public IList<string> Features { get; }
+        public bool GZipEnabled { get; }
+        public string ApiKey { get; }
+        public string ApplicationName { get; }
+        public ISerializer Serializer { get; }
+    }
+}
